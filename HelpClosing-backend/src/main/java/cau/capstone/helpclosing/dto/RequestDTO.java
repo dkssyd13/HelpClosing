a946bae3971f90945b34559b3472ece9e7759bc2
advicewook <ceo0818@gmail.com>
2023-11-01 00:01:03 +0900

    추가

---
+++ HelpClosing-backend/src/main/java/cau/capstone/helpclosing/dto/RequestDTO.java
@@ -0,0 +1,25 @@
+package cau.capstone.helpclosing.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RequestDTO {
+    private String targetToken;
+    private String title;
+    private String body;
+
+    @JsonCreator
+    private RequestDTO(@JsonProperty("targetToken") String targetToken,@JsonProperty("title") String title,@JsonProperty("body") String body) {
+        this.targetToken = targetToken;
+        this.title = title;
+        this.body = body;
+    }
+
+}
---
+++ HelpClosing-backend/src/main/java/cau/capstone/helpclosing/service/FCMService.java
@@ -0,0 +1,71 @@
+package cau.capstone.helpclosing.service;
+
+
+import cau.capstone.helpclosing.FcmMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.common.net.HttpHeaders;
+import com.google.firebase.messaging.Message;
+
+import com.google.gson.JsonParseException;
+import lombok.RequiredArgsConstructor;
+import okhttp3.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+
+import java.io.IOException;
+import java.util.List;
+
+
+@Component
+@RequiredArgsConstructor
+public class FCMService {
+
+    private final String API_URL = "https://fcm.googleapis.com/v1/projects/cpastone-cau-helpclosing/messages:send";
+    private final ObjectMapper objectMapper;
+
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+        String message = makeMessage(targetToken, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println(response.body().string());
+    }
+
+    private String makeMessage(String targetToken, String title, String body) throws JsonParseException, JsonProcessingException{
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder()
+                        .token(targetToken)
+                        .notification(FcmMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build()
+                        ).build()).validateOnly(false).build();
+
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
+    private String getAccessToken() throws IOException{
+        String firebaseConfigPath = "firebase/serviceAccountKey.json";
+
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
+}
