c5a10b932ed82263d6975cc835c5d3b455297522
jooyeon <juyeon0716@naver.com>
2023-11-14 19:54:47 +0900

    gradle에 jjwt 추가

--- HelpClosing-backend/src/main/java/cau/capstone/helpclosing/security/entity/JwtUtil.java
+++ HelpClosing-backend/src/main/java/cau/capstone/helpclosing/security/entity/JwtUtil.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.conscrypt.ct.DigitallySigned;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
@@ -66,7 +67,11 @@ public class JwtUtil {
         return createToken(claims, email);
     }
 
+    //@Value("${jwt.secret.key}")
+    //private static final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
     private String createToken(Map<String, Object> claims, String email) {
+
         return Jwts.builder()
                 .setClaims(claims)  // token에 담을 정보
                 .setSubject(email)  // jwt 토큰 제목
